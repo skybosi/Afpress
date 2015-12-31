cc749ce93626816b00ffc6298a6baa20e62b2653
skybosi <skybosi_fxc@163.com>
2015-12-31 09:37:50 +0800

    deal with source

--- 7.29/Ac程序控制追踪/cctrl/src/c_ctrl.c
+++
@@ -1,5 +0,0 @@
-#include"../include/c_ctrl.h"
-int main(int argc, char *argv[])
-{
-	return 0;
-}
--- Ac程序控制追踪/fmtcsource/test/hello1.c
+++
@@ -1,9 +0,0 @@
-#include"test.h"
-int main(int argc, char *argv[])
-{//打印hello字接口起床了就是咯你了
-//解太可怜了符串
-/*ghijcghj*/
-	system("pwd");
-	printf("hello ");
-	return 0;
-}
